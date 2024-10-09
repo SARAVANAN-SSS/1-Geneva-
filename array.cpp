11b5744a02cd5505153a6ce9cd7c869dcd8660ab
Saravanan S <85794832+SARAVANAN-SSS@users.noreply.github.com>
2024-10-09 07:17:34 +0000

    feat: binary search

--- array.cpp
+++ array.cpp
@@ -1,13 +1,61 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+
+//     // 08/10/2024: Array: 
+
+//     // Insertion Sort
+
+//     int n;
+
+//     cin>>n;
+    
+//     cout<<"Start why not "<<n;
+
+//     return 0;
+// }
+
+
+// -------------------------------------------------------------------------------------
+
+
+// 09/10/2024 - Binary Search Home Work - TC - log n
+
+
 #include <iostream>
 using namespace std;
 
-int main() {
-
-    int n;
-
-    cin>>n;
+int binarySearch(int arr[], int n, int key){
     
-    cout<<"Start why not "<<n;
+    int start = 0, end = n-1, mid;
+    
+    while(start<=end){
+        mid = start+(end-start/2);
+        
+        if(arr[mid]==key){
+            return mid;
+        }else if(arr[mid]<key){
+            end = mid-1;
+        }else
+        start = mid+1;
+    }
+    
+    return -1;
+    
+}
+
+int main() {
+    
+    int arr[1000] = {30, 28, 20, 16, 4};
+    
+    int n = 5;
+    
+    int key = 20;
+    
+    cout<<"The Answer of Given Binary Search is "<<binarySearch(arr, n, key)<<endl<<endl<<endl;
+    
 
     return 0;
 }
